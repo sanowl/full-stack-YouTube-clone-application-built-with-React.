d895b1df666e1935837f4e16c0242db4edcda19a
san.owl <99511815+sanowl@users.noreply.github.com>
2024-02-03 20:44:26 +0300

    kjlk

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
-import { Stack } from "@mui/material";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Stack, AppBar, Toolbar, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-import { logo } from "../utils/constants";
-import { SearchBar } from "./";
+import { logo } from '../utils/constants';
+import { SearchBar } from './';
 
 const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
-    </Link>
-    <SearchBar />
-  </Stack>
+  <AppBar position="sticky" elevation={0} sx={{ background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)' }}>
+    <Toolbar>
+      <Stack direction="row" alignItems="center" width="100%" justifyContent="space-between" sx={{ py: 1 }}>
+        <Link to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
+          <img src={logo} alt="logo" height="45" />
+        </Link>
+        <SearchBar />
+      </Stack>
+    </Toolbar>
+  </AppBar>
 );
 
 export default Navbar;
